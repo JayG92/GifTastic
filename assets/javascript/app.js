d0e684b337daafc469901575aab6cfd684c330a5
JayG <jaypguevara@gmail.com>
2019-07-28 11:16:05 -0700

    Added Css

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// <!-- load gify src url here -->
+// <!-- end of gify URL -->
+// <!-- Steps -->
+// <!-- add an event listener code to buttons -->
+// <!-- Add a value to a button -->
+// <!-- and Variables to data nad queryURL -->
+// <!-- add ajax to request witth the query URL -->
+// <!-- add a .then function  -->
+// <!--  -->
+$(document).ready(function(){
+    var cars = [];
+
+
+
+
+
+    
+
+})
+
+
+
