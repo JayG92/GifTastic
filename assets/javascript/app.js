8981c37dc36f5a01ebc86ade2d0f9c09d79905bb
JayG <jaypguevara@gmail.com>
2019-07-28 12:12:59 -0700

    submit btn takes input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,16 +7,36 @@
 // <!-- add ajax to request witth the query URL -->
 // <!-- add a .then function  -->
 // <!--  -->
-$(document).ready(function(){
-    var cars = [];
+$(document).ready(function () {
+    // global variables
+    let cars = [];
+    let $input = $('#input');
+    let $submit = $('#submit');
+
+    // get value when user clicks on submit button
+    $submit.on('click', function (event) {
+        // prevents from clearing out after hitting submit
+        event.preventDefault();
+        console.log($input.val());
+        // grabs value when entered in input
+        $input.val();
+
+
+    })
+    // end of event submit event
+
+
+
+    // create a "GET" request from API
 
 
 
 
 
-    
 
+    // end of code
 })
+
 
 
 
